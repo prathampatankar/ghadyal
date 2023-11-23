d497e62e2c13bb3c02fb91dd0bf591d30e775647
Pratham Patankar <83165113+prathampatankar@users.noreply.github.com>
2023-11-23 20:00:09 +0530

    v1

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+importScripts('cache-polyfill.js');
+
+let CACHE_VERSION = 'app-v0.00';
+// give all files path you want to work offline
+let CACHE_FILES = [
+  './',
+  'ghadyal.html',
+  'ghadyallogo.svg',
+  'cache-polyfill.js',
+];
+
+self.addEventListener('install', function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(CACHE_FILES);
+            })
+    )
+})
+
+self.addEventListener('fetch', function (event) {
+    let online = navigator.onLine
+    if (!online) {
+        event.respondWith(
+            caches.match(event.request).then(function (res) {
+                if (res) {
+                    return res;
+                }
+            })
+        )
+    }
+})
+
+self.addEventListener('activate', function(event){
+    event.waitUntil(
+        caches.keys().then(function(keys){
+            return prompt.all(keys.map(function(keys, i){
+                if(keys !== CACHE_VERSION){
+                    return caches.delete(keys[i]);
+                }
+            }))
+        })
+    )
+})
